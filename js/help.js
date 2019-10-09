518da0a6ed96914549537fd4adc236c994c06f15
Euan <euanriggans1@gmail.com>
2019-10-09 19:24:40 +0100

    Improved help page links and fix broken links

--- js/help.js
+++ js/help.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
         alert("Unable to close window due to Firefox security policy. Please close this window manually.");
         // window.close();
     });
+    $('#openChangelog').click(function () {
+        browser.tabs.create({
+            active: true,
+            'url': chrome.extension.getURL('changelog.html')
+        });
+    });
 });
